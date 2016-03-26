91ff343ee8c1a8a4e3f275118695674206d35636
Brian Koech <brnkirwa@gmail.com>
2016-03-26 22:06:50 +0300

    Add morgan and body-parser modules

--- scripts/web-server.js
+++ scripts/web-server.js
@@ -2,7 +2,11 @@ var express = require('express');
 var path = require('path');
 var app = express();
 var rootPath = path.normalize(__dirname + '/../');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
+app.use(morgan('dev'));
 app.use(express.static(rootPath + '/app'));
 
 app.listen(8000);
