612eed8e42fca8e460c3a09963ef3d9dcc2dc9cb
Brian Koech <brnkirwa@gmail.com>
2016-03-28 23:46:19 +0300

    Catch error on listen failure

--- scripts/web-server.js
+++ scripts/web-server.js
@@ -9,6 +9,12 @@ app.use(bodyParser.json());
 app.use(morgan('dev'));
 app.use(express.static(rootPath + '/app'));
 
-app.listen(8000);
+app.listen(8000, function(err) {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log('connected!');
+  }
+});
 
 console.log('Listening on port 8000....');
