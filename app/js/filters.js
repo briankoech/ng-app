7efe1a0338655cf6abd3e2ab048d6ec6de7b8a34
Brian Koech <brnkirwa@gmail.com>
2016-04-03 23:00:02 +0300

    Add closures to filters

--- app/js/filters.js
+++ app/js/filters.js
@@ -1,22 +1,24 @@
-'use strict';
+(function() {
+  'use strict';
 
-eventsApp.filter('durations', function() {
-  return function(duration) {
-    switch(duration) {
-      case 1:
-        return 'Half day';
+  eventsApp.filter('durations', function() {
+    return function(duration) {
+      switch(duration) {
+        case 1:
+          return 'Half day';
+          break;
+        case 2:
+          return '1 Hour';
         break;
-      case 2:
-        return '1 Hour';
-      break;
-      case 3:
-        return 'Half Day';
-      break;
-      case 4:
-        return 'Full Day';
-      break;
-      default:
+        case 3:
+          return 'Half Day';
         break;
+        case 4:
+          return 'Full Day';
+        break;
+        default:
+          break;
+      }
     }
-  }
-})
+  });
+})();
