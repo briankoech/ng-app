6108d724adcdebd3fd818345afdae715fd935dfe
Brian Koech <brnkirwa@gmail.com>
2016-04-06 14:14:44 +0300

    Add  and  service examples

---
+++ app/js/controllers/LocaleSampleController.js
@@ -0,0 +1,10 @@
+(function() {
+  'use strict';
+
+  eventsApp.controller('LocaleSampleController', 
+    function($scope, $locale) {
+
+      $scope.myDate = Date.now();
+      $scope.myFormat = $locale.DATETIME_FORMATS.fullDate;
+    });
+})();
