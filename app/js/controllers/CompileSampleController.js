c307fbaf0ba43f5db10ae91fb4b0b4d9b415b3b4
Brian Koech <brnkirwa@gmail.com>
2016-04-06 10:47:39 +0300

    Add  service example

---
+++ app/js/controllers/CompileSampleController.js
@@ -0,0 +1,8 @@
+(function() {
+  'use strict';
+  eventsApp.controller('CompileSampleController', function($scope, $compile) {
+    $scope.appendDivToElement = function(markup) {
+      return $compile(markup) ($scope).appendTo(angular.element('#appendHere'));
+    };
+  });
+})();
